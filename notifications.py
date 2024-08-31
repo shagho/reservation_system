a019ddaca9fe3b2cda5383b9a51371ac4b1a1ec1
navid <navid.shagho003@gmail.com>
2024-08-31 12:11:24 +0330

    add kavenegar notification

---
+++ notifications.py
@@ -0,0 +1,33 @@
+from kavenegar import *
+
+from django.conf import settings
+
+
+
+class KavenegarSMS:
+
+    def __init__(self):
+        self.api = KavenegarAPI(settings.KAVENEGAR_API_KEY)
+
+    def register(self, receptor=None, code=None, password=None):
+        self.params = {
+            'receptor': receptor,
+            'template': 'otp-code-res',
+            'token': code,
+            'type': 'sms'
+        }
+    
+    def send(self):
+        flag = True
+        for i, j in self.params.items():
+            if j is None:
+                flag = False
+        if flag:
+            try:
+                return self.api.verify_lookup(self.params)
+            except APIException as e:
+                return e
+            except HTTPException as e:
+                return e
+        else:
+            raise APIException
