97401022995cbcfb44acd93bca764c3d5172f9fa
navid <navid.shagho003@gmail.com>
2024-09-28 08:22:30 +0330

    add forms changes

--- serve/forms.py
+++ serve/forms.py
@@ -9,10 +9,12 @@ from jalali_date_new.widgets import AdminJalaliDateWidget, AdminJalaliTimeWidget
 class ProfileForm(forms.Form):
     name = forms.CharField(max_length=128, required=True, error_messages={'required': 'این فیلد الزامی است.'})
     email = forms.EmailField(max_length=128, required=False)
+    company_name = forms.CharField(max_length=128, required=False)
 
     def save(self, user, data):
         user.email = data['email']
         user.name = data['name']
+        user.company_name = data['company_name']
         user.save()
 
 
